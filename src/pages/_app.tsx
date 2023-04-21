f323cad92c8207053cfce9a8c12c3fcfe9b4b6f3
Masayasu Fujiwara <ma38su@gmail.com>
2023-04-21 22:56:20 +0900

    add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider withGlobalStyles>
       <Component {...pageProps} />
+      <Analytics />
     </MantineProvider>
   );
 }
